b324d6705f9809a4f46da2625013469bf77e5aab
kneelesh951 <kneelesh951@gmail.com>
2024-10-28 12:18:13 +0100

    modifications in yml file

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,23 +1,23 @@
 variable "in_bucket_name" {
   description = "Name of the input S3 bucket"
   type        = string
-  default     = "test-in"  # Set default value
+  default     = "test-in-oct24"  # Set default value
 }
 
 variable "out_bucket_name" {
   description = "Name of the output S3 bucket"
   type        = string
-  default     = "test-out"  # Set default value
+  default     = "test-out-oct24"  # Set default value
 }
 
 variable "tmp_bucket_name" {
   description = "Name of the temporary S3 bucket"
   type        = string
-  default     = "test-tmp"  # Set default value
+  default     = "test-tmp-oct24"  # Set default value
 }
 
 variable "export_bucket_name" {
   description = "Name of the export S3 bucket"
   type        = string
-  default     = "test-export"  # Set default value
+  default     = "test-export-oct24"  # Set default value
 }
