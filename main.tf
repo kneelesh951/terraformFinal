75d57f2cba7324dcf7d89b7e0d3c01d44a883157
kneelesh951 <kneelesh951@gmail.com>
2024-10-30 18:57:08 +0100

    structural changes

---
+++ main.tf
@@ -0,0 +1,12 @@
+# root/main.tf
+
+# Define the environment variable to choose which environment to deploy
+variable "environment" {
+  description = "The environment to deploy (dev, sit, prod)"
+  type        = string
+}
+
+# Load environment-specific configurations based on the environment variable
+module "environment" {
+  source = "./env/${var.environment}"  # Use the environment variable to point to the correct folder
+}
