06b8e99663eeffbb8a602acb5cabadccaca622d5
kneelesh951 <kneelesh951@gmail.com>
2024-10-31 18:24:31 +0100

    added conditional checks for s3 buckets in outputs.tf and main.tf

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -1,44 +1,36 @@
-# modules/s3/main.tf
-
-# Check if the input S3 bucket already exists
+# Check for existing S3 buckets
 data "aws_s3_bucket" "existing_in_bucket" {
   bucket = var.in_bucket_name
 }
 
-# Create the input S3 bucket if it does not exist
+data "aws_s3_bucket" "existing_out_bucket" {
+  bucket = var.out_bucket_name
+}
+
+data "aws_s3_bucket" "existing_tmp_bucket" {
+  bucket = var.tmp_bucket_name
+}
+
+data "aws_s3_bucket" "existing_export_bucket" {
+  bucket = var.export_bucket_name
+}
+
+# Create S3 buckets only if they do not already exist
 resource "aws_s3_bucket" "in_bucket" {
   count  = data.aws_s3_bucket.existing_in_bucket.id == "" ? 1 : 0
   bucket = var.in_bucket_name
 }
 
-# Check if the output S3 bucket already exists
-data "aws_s3_bucket" "existing_out_bucket" {
-  bucket = var.out_bucket_name
-}
-
-# Create the output S3 bucket if it does not exist
 resource "aws_s3_bucket" "out_bucket" {
   count  = data.aws_s3_bucket.existing_out_bucket.id == "" ? 1 : 0
   bucket = var.out_bucket_name
 }
 
-# Check if the temporary S3 bucket already exists
-data "aws_s3_bucket" "existing_tmp_bucket" {
-  bucket = var.tmp_bucket_name
-}
-
-# Create the temporary S3 bucket if it does not exist
 resource "aws_s3_bucket" "tmp_bucket" {
   count  = data.aws_s3_bucket.existing_tmp_bucket.id == "" ? 1 : 0
   bucket = var.tmp_bucket_name
 }
 
-# Check if the export S3 bucket already exists
-data "aws_s3_bucket" "existing_export_bucket" {
-  bucket = var.export_bucket_name
-}
-
-# Create the export S3 bucket if it does not exist
 resource "aws_s3_bucket" "export_bucket" {
   count  = data.aws_s3_bucket.existing_export_bucket.id == "" ? 1 : 0
   bucket = var.export_bucket_name
--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,23 +1,23 @@
 variable "in_bucket_name" {
   description = "Name of the input S3 bucket"
   type        = string
-  default     = "test-in-oct24"  
+  default     = "test-in-oct24"
 }
 
 variable "out_bucket_name" {
   description = "Name of the output S3 bucket"
   type        = string
-  default     = "test-out-oct24"  
+  default     = "test-out-oct24"
 }
 
 variable "tmp_bucket_name" {
   description = "Name of the temporary S3 bucket"
   type        = string
-  default     = "test-tmp-oct24"  
+  default     = "test-tmp-oct24"
 }
 
 variable "export_bucket_name" {
   description = "Name of the export S3 bucket"
   type        = string
-  default     = "test-export-oct24-31oct"  
+  default     = "test-export-oct24"
 }
