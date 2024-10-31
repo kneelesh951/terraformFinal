def687fb477829860a5ac7311f090e5d06333dc7
kneelesh951 <kneelesh951@gmail.com>
2024-10-31 17:33:21 +0100

    added conditional checks for s3 buckets

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -1,15 +1,45 @@
-resource "aws_s3_bucket" "in_bucket" {
+# modules/s3/main.tf
+
+# Check if the input S3 bucket already exists
+data "aws_s3_bucket" "existing_in_bucket" {
   bucket = var.in_bucket_name
 }
 
-resource "aws_s3_bucket" "out_bucket" {
+# Create the input S3 bucket if it does not exist
+resource "aws_s3_bucket" "in_bucket" {
+  count  = data.aws_s3_bucket.existing_in_bucket.id == "" ? 1 : 0
+  bucket = var.in_bucket_name
+}
+
+# Check if the output S3 bucket already exists
+data "aws_s3_bucket" "existing_out_bucket" {
   bucket = var.out_bucket_name
 }
 
-resource "aws_s3_bucket" "tmp_bucket" {
+# Create the output S3 bucket if it does not exist
+resource "aws_s3_bucket" "out_bucket" {
+  count  = data.aws_s3_bucket.existing_out_bucket.id == "" ? 1 : 0
+  bucket = var.out_bucket_name
+}
+
+# Check if the temporary S3 bucket already exists
+data "aws_s3_bucket" "existing_tmp_bucket" {
   bucket = var.tmp_bucket_name
 }
 
+# Create the temporary S3 bucket if it does not exist
+resource "aws_s3_bucket" "tmp_bucket" {
+  count  = data.aws_s3_bucket.existing_tmp_bucket.id == "" ? 1 : 0
+  bucket = var.tmp_bucket_name
+}
+
+# Check if the export S3 bucket already exists
+data "aws_s3_bucket" "existing_export_bucket" {
+  bucket = var.export_bucket_name
+}
+
+# Create the export S3 bucket if it does not exist
 resource "aws_s3_bucket" "export_bucket" {
+  count  = data.aws_s3_bucket.existing_export_bucket.id == "" ? 1 : 0
   bucket = var.export_bucket_name
 }
