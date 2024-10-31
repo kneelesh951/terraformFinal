1cab3a5c4b850b7aba1ddc47ab86368cc8ef2bd3
kneelesh951 <kneelesh951@gmail.com>
2024-10-31 17:44:39 +0100

    added conditional checks for s3 buckets in outputs.tf

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,19 +1,21 @@
+# modules/s3/outputs.tf
+
 output "in_bucket_arn" {
   description = "The ARN of the input S3 bucket"
-  value       = aws_s3_bucket.in_bucket.arn
+  value       = try(aws_s3_bucket.in_bucket[0].arn, null)  # Safely access, return null if not created
 }
 
 output "out_bucket_arn" {
   description = "The ARN of the output S3 bucket"
-  value       = aws_s3_bucket.out_bucket.arn
+  value       = try(aws_s3_bucket.out_bucket[0].arn, null)  # Safely access, return null if not created
 }
 
 output "tmp_bucket_arn" {
   description = "The ARN of the temporary S3 bucket"
-  value       = aws_s3_bucket.tmp_bucket.arn
+  value       = try(aws_s3_bucket.tmp_bucket[0].arn, null)  # Safely access, return null if not created
 }
 
 output "export_bucket_arn" {
   description = "The ARN of the export S3 bucket"
-  value       = aws_s3_bucket.export_bucket.arn
+  value       = try(aws_s3_bucket.export_bucket[0].arn, null)  # Safely access, return null if not created
 }
