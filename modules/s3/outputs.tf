4d06d2917102ee38e6cd33bc4adb03e92495315a
kneelesh951 <kneelesh951@gmail.com>
2024-11-01 13:04:51 +0100

    removed checks from s3 module

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,23 +1,15 @@
-# Output for the ARN of the input S3 bucket
 output "in_bucket_arn" {
-  value = length(aws_s3_bucket.in_bucket) > 0 ? aws_s3_bucket.in_bucket[0].arn : "Input Bucket not created"
-  description = "The ARN of the input S3 bucket."
+  value = aws_s3_bucket.in_bucket.arn  # ARN of the input bucket
 }
 
-# Output for the ARN of the output S3 bucket
 output "out_bucket_arn" {
-  value = length(aws_s3_bucket.out_bucket) > 0 ? aws_s3_bucket.out_bucket[0].arn : "Output Bucket not created"
-  description = "The ARN of the output S3 bucket."
+  value = aws_s3_bucket.out_bucket.arn  # ARN of the output bucket
 }
 
-# Output for the ARN of the temporary S3 bucket
 output "tmp_bucket_arn" {
-  value = length(aws_s3_bucket.tmp_bucket) > 0 ? aws_s3_bucket.tmp_bucket[0].arn : "Temporary Bucket not created"
-  description = "The ARN of the temporary S3 bucket."
+  value = aws_s3_bucket.tmp_bucket.arn  # ARN of the temporary bucket
 }
 
-# Output for the ARN of the export S3 bucket
 output "export_bucket_arn" {
-  value = length(aws_s3_bucket.export_bucket) > 0 ? aws_s3_bucket.export_bucket[0].arn : "Export Bucket not created"
-  description = "The ARN of the export S3 bucket."
+  value = aws_s3_bucket.export_bucket.arn  # ARN of the export bucket
 }
